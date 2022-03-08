1ae966ff976dc2cc5450345362eedeeab52d56a8
Jonasz Balinski <->
2022-03-08 08:56:11 +0100

    Add basic method for initialize grid

--- Minesweeper/Form1.Designer.cs
+++ Minesweeper/Form1.Designer.cs
@@ -28,13 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gamePanel.Location = new System.Drawing.Point(0, 0);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(782, 753);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
     }
 }
 
--- Minesweeper/Form1.cs
+++ Minesweeper/Form1.cs
@@ -15,6 +15,28 @@ namespace Minesweeper
         public Form1()
         {
             InitializeComponent();
+            InitializeGrid(20, 20);
+        }
+
+        private void InitializeGrid(uint x, uint y)
+        {
+            //float scaleX = (this.Width / x); 
+            //float scaleY = (this.Height / y); 
+
+            int scaleX = (int)(800 / x); 
+            int scaleY = (int)(800 / y); 
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Button btn = new Button();
+                    btn.Size = new Size((int)scaleX, (int)scaleY);
+                    btn.Location = new Point((int)(i*scaleX), (int)(j*scaleY));
+                    btn.Click += (s, e) => { btn.BackColor = Color.DarkBlue; };
+                    this.gamePanel.Controls.Add(btn);
+                }
+            }
         }
     }
 }
