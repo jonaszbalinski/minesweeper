7ed5a19d6c5904a5f1c5d27df8ce868fea38c5bc
Jonasz Balinski <->
2022-03-08 09:27:03 +0100

    Add bomb system

--- Minesweeper/Form1.Designer.cs
+++ Minesweeper/Form1.Designer.cs
@@ -35,6 +35,7 @@
             // 
             this.gamePanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gamePanel.Location = new System.Drawing.Point(0, 0);
+            this.gamePanel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
             this.gamePanel.Name = "gamePanel";
             this.gamePanel.Size = new System.Drawing.Size(782, 753);
             this.gamePanel.TabIndex = 0;
@@ -45,6 +46,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(782, 753);
             this.Controls.Add(this.gamePanel);
+            this.MaximumSize = new System.Drawing.Size(800, 800);
+            this.MinimumSize = new System.Drawing.Size(800, 800);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Minesweeper/Form1.cs
+++ Minesweeper/Form1.cs
@@ -12,30 +12,66 @@ namespace Minesweeper
 {
     public partial class Form1 : Form
     {
+        private Button[,] gameFields;
+        private bool[,] bombPositions;
+        private int fieldSize = 20;
         public Form1()
         {
             InitializeComponent();
+
+
             InitializeGrid(20, 20);
         }
 
-        private void InitializeGrid(uint x, uint y)
+        private void InitializeGrid(int x, int y)
         {
+            Random random = new Random();
+            
+            gameFields = new Button[x, y];
+            bombPositions = new bool[x, y];
+
             //float scaleX = (this.Width / x); 
             //float scaleY = (this.Height / y); 
-
-            int scaleX = (int)(800 / x); 
-            int scaleY = (int)(800 / y); 
+            int scaleX = (int)(400 / x); 
+            int scaleY = (int)(400 / y); 
 
             for (int i = 0; i < x; i++)
             {
                 for (int j = 0; j < y; j++)
                 {
                     Button btn = new Button();
-                    btn.Size = new Size((int)scaleX, (int)scaleY);
-                    btn.Location = new Point((int)(i*scaleX), (int)(j*scaleY));
-                    btn.Click += (s, e) => { btn.BackColor = Color.DarkBlue; };
+                    gameFields[i, j] = btn;
+                    btn.Size = new Size(fieldSize, fieldSize);
+                    btn.Margin = new Padding(0, 0, 0, 0);
+                    btn.Location = new Point(i*fieldSize, j*fieldSize);
+                    btn.Click += (s, e) => { 
+                        if (s is Button)
+                        {
+                           Button b = s as Button;
+                           if(bombPositions[(int)b.Location.X/fieldSize, (int)b.Location.Y/fieldSize])
+                           {
+                                b.BackColor = Color.Red;
+                           }
+                           else
+                           {
+                                b.BackColor = Color.Green;
+                           }
+                        }
+                    };
                     this.gamePanel.Controls.Add(btn);
+
+                    bombPositions[i, j] = false;
                 }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                int randX = random.Next(x);
+                int randY = random.Next(y);
+
+                if (!bombPositions[randX, randY])
+                    bombPositions[randX, randY] = true;
+                else i--;
             }
         }
     }
